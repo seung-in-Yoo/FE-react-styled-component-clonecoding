aee6e506eaf1cdffb28e86483ff7c0573ba7f5bf
Yoo <tmddls91@inha.edu>
2024-05-13 11:08:58 +0900

    pages

---
+++ src/pages/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "../components/Header";
+import Center from "../components/Center";
+import Card from "../components/Card";
+
+
+function App() {
+    return ( 
+      <>
+        <Header></Header>
+        <Center></Center>
+        <Card></Card>
+      </>
+  );
+}
+
+export default App;
+
